381ea2cbe08ff689b21c90c8aee421be389058ff
Dealer63636j <dealersausage@gmail.com>
2025-06-24 18:44:53 +0530

    lab 3

---
+++ lab3/avgOfThree.java
@@ -0,0 +1,9 @@
+class avgOfThree {
+	public static void main(String[] args) {
+    		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		int c=Integer.parseInt(args[2]);
+		System.out.println("The sum of three numbers is: " + ((a+b+c)/3));
+
+	}
+}
