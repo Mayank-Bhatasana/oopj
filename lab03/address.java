2ac2fb55682a52a6fdb2c824fd5ceddd42d5b178
Mayank-Bhatasana <bhatasanamayank06@gmail.com>
2025-07-08 13:01:28 +0530

    lab 3

---
+++ lab03/address.java
@@ -0,0 +1,7 @@
+class address{
+	public static void main(String [] args){
+		System.out.println("08, kanya chatraly road");
+		System.out.println("sanara road");
+		System.out.println("Morbi: 363641");
+	}
+}
---
+++ lab03/sum.java
@@ -0,0 +1,7 @@
+class sum {
+    public static void main(String[] args) {
+	int a=Integer.parseInt(args[0]);
+	int b=Integer.parseInt(args[1]);
+        System.out.print("The out put is: " + (a+b));
+    }
+}
