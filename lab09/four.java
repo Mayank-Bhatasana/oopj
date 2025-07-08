426a0b18681a786d85da48b809d23f8617a40e63
Mayank-Bhatasana <bhatasanamayank06@gmail.com>
2025-07-08 13:02:05 +0530

    lab 9

---
+++ lab09/four.java
@@ -0,0 +1,16 @@
+// 4. WAP to print given number in reverse order. (C)
+import java.util.Scanner;
+class four{
+    public static void main(String[] s){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Give a input");
+        int n = sc.nextInt(),rev=0;
+
+        while(n!=0){
+            int temp=n%10;
+            n/=10;
+            rev = rev * 10 + temp;
+        }
+        System.out.println("The output is "+rev);
+    }
+}
